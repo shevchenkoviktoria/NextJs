e53d9ceea52b54c06d1d5d30c47c7e98637680fa
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2023-10-16 14:06:43 +0300

    added first part

---
+++ app/prediction/[name]/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Page({ params }:any ) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {params.name}
+    </main>
+  )
+}
