17335fe4e5bbb93e4e9b9e4b1cf1958ff08dc195
Viktoriia Shevchenko <viktoriiashevchenko@Viktoriias-MacBook-Pro.local>
2025-02-10 19:42:14 +0200

    fixes

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,32 +1,32 @@
-
-
 const getPredictedAge = async (name: string) => {
-  const res = await fetch(`https://api.agify.io/?name=${name}`)
-  return res.json()
+  const res = await fetch(`https://api.agify.io/?name=${name}`);
+  return res.json();
+};
 
-}
 const getPredictedGender = async (name: string) => {
-  const res = await fetch(`https://api.genderize.io/?name=${name}`)
-  return res.json()
+  const res = await fetch(`https://api.genderize.io/?name=${name}`);
+  return res.json();
+};
 
-}
 const getPredictedCountry = async (name: string) => {
-  const res = await fetch(`https://api.nationalize.io/?name=${name}`)
-  return res.json()
-
-}
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+  return res.json();
+};
 
 interface Params {
-  params: { name: string }
+  params: { name: string };
 }
 
-export default async function Page({ params }: Params ) {
+export default async function Page({ params }: Params) {
+  const ageDataPromise = getPredictedAge(params.name);
+  const genderDataPromise = getPredictedGender(params.name);
+  const countryDataPromise = getPredictedCountry(params.name);
 
-  const ageData = getPredictedAge(params.name)
-  const genderData = getPredictedGender(params.name)
-  const countryData = getPredictedCountry(params.name)
-
-  const [age, gender, country] = await Promise.all([ageData, genderData, countryData])
+  const [age, gender, country] = await Promise.all([
+    ageDataPromise,
+    genderDataPromise,
+    countryDataPromise,
+  ]);
 
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
@@ -35,15 +35,15 @@ export default async function Page({ params }: Params ) {
           Personal Info
         </div>
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
-          Age: {age?.age}
+          Age: {age?.age ?? "Unknown"}
         </div>
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
-          Gender: {gender?.gender}
+          Gender: {gender?.gender ?? "Unknown"}
         </div>
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
-          Nationality: {country?.country[0]?.country_id}
+          Nationality: {country?.country?.[0]?.country_id ?? "Unknown"}
         </div>
       </div>
     </div>
-  )
+  );
 }
